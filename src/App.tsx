552779213ea6d617d7ec257381ae2c0c79f25970
Jose Jerez <josejerez2296@gmail.com>
2023-12-21 13:03:43 +0000

    chore: install tailwind and daisyui

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+    return (
+
+        <div className='main-container h-screen bg-base-300 flex flex-col'>
+            <main className='max-w-5xl w-full h-screen mx-auto grid gap-4 grid-cols-1'>
+                {/* <Outlet /> */}
+                <h1 className="text-3xl font-bold underline">
+                    Hello world!
+                </h1>
+            </main>
+        </div>
+    )
+}
+
+export default App
